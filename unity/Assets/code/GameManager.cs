a71196775b4fbe1b10e670838c3fbddeda0ee457
myungseo++ <84305921+softwere123@users.noreply.github.com>
2023-09-13 15:48:32 +0900

    Feat: "SGTransformExtention 구현"
    전 주차까지 다 했습니다

---
+++ unity/Assets/code/GameManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int playerScore = 0;
+
+    public void Inscrease
+    {
+
+    }
+}
